53e69b93f4a6082444781c4060b7c92a18524dde
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2024-05-28 13:26:50 -0600

    cambios en index

---
+++ test/example.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Línea horizontal con CSS</title>
+    <style>
+        .linea-horizontal {
+            width: 10%;
+            /* Ancho del contenedor */
+            height: 8px;
+            /* Grosor de la línea */
+            background: linear-gradient(to right,  #BC955C 33%, transparent 33%, transparent 100%);
+        }
+    </style>
+</head>
+
+<body>
+    <div class="linea-horizontal"></div>
+</body>
+
+</html>
