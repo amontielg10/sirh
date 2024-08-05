7edcbb34396eb1844a19f83a03f145955fd7bfb2
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2024-08-05 17:03:10 -0600

    more chnages

--- App/View/Central/Modulo/IncidenciasM/index.php
+++ App/View/Central/Modulo/IncidenciasM/index.php
@@ -1,6 +1,6 @@
 <div class="div-spacing">
     <div class="row">
-        <div class="col-2">
+        <div class="col-12 col-md-6 col-lg-6 col-xl-2 mb-2">
             <div class="div-spacing"></div>
             <div class="card font-size-modulo shadow-lg">
                 <h6 class="card-header text-center background-modal color-text-tittle">Tipo de incidencia</h6>
@@ -9,19 +9,17 @@
                     <a onclick="buscarRetardo();" class="nav-link-mod active" id="v-tabs-home-tab" data-toggle="pill"
                         href="#v-tabs-home" role="tab" aria-controls="v-tabs-home" aria-selected="true">
                         <i class="fa fa-folder-open mr-2"></i> Retardos</a>
-
                     <a onclick="buscarFalta();" class="nav-link-mod" id="v-tabs-profile-tab" data-toggle="pill"
                         href="#v-tabs-profile" role="tab" aria-controls="v-tabs-profile" aria-selected="false">
                         <i class="fa fa-folder-open mr-2"></i> Faltas</a>
                     <a onclick="buscarLicencia();" class="nav-link-mod" id="v-tabs-messages-tab" data-toggle="pill" href="#v-tabs-messages"
                         role="tab" aria-controls="v-tabs-messages" aria-selected="false">
-                        <i class="fa fa-folder-open mr-2"></i> Licencia médica</a>
-
+                        <i class="fa fa-folder-open mr-2"></i> Licencias</a>
                 </div>
             </div>
         </div>
 
-        <div class="col-10">
+        <div class="col-12 col-md-12 col-lg-12 col-xl-10 mb-10">
             <div class="tab-content" id="v-tabs-tabContent">
                 <div class="tab-pane fade show active" id="v-tabs-home" role="tabpanel"
                     aria-labelledby="v-tabs-home-tab">
@@ -57,7 +55,7 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="card font-size-modulo shadow-lg">
-                                    <h5 class="card-header text-center background-modal color-text-tittle">Licencias m&eacutedicas</h5>
+                                    <h5 class="card-header text-center background-modal color-text-tittle">Licencias</h5>
                                     <div class="card-body">
                                         <?php include 'Licencias/index.php' ?>
                                     </div>
