916d62bb8eabaa92cd556981303a2fc1c1837322
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2024-09-03 19:47:16 -0600

    more chnages

--- App/Controllers/Central/IncidenciasC/ValoresC.php
+++ App/Controllers/Central/IncidenciasC/ValoresC.php
@@ -10,28 +10,54 @@ $fechaInicio = $_POST['fecha_inicio_ins'];
 $fechaFin = $_POST['fecha_fin_ins'];
 $idEmpleado = $_POST['id_tbl_empleados_hraes'];
 
+$fechaInicio_date = new DateTime($fechaInicio);
+$fechaFin_date = new DateTime($fechaFin);
+
 $periodo = 'SIN RESULTADO';
 $diasSeleccionados = 'SIN RESULTADO';
+$diasRestantes = 'SIN RESULTADO';
 
 //Se obtiene el periodo
 if ($fechaInicio != '') {
     if (pg_num_rows($catDiasM->getPeriodo($fechaInicio)) > 0) {
-        $periodo = $row->returnArrayById($catDiasM->getPeriodo($fechaInicio));
-        $periodo = $periodo[0];
+        $periodoResult = $row->returnArrayById($catDiasM->getPeriodo($fechaInicio));
+        $periodo = $periodoResult[0];
+        // calculo de dias seleccionados
+        $allDays = getAllDays($periodoResult[1], $periodoResult[2], $idEmpleado);
+
+        if ($fechaInicio != '' && $fechaFin == '') {//funion de un solo dia
+            $diasSeleccionados = '1 Día'; //uno perteneciendo a solo un dia que se selecciono
+            $diasRestantes = $allDays - 1;
+        } else if ($fechaInicio != '' && $fechaFin != '') { //funcion donde se selecciona un rango de fechas
+            if ($fechaInicio < $fechaFin) { // validacion que las fechas esten correctas, la de inicio ser menor que la  fecha fin
+                $intervalo = $fechaInicio_date->diff($fechaFin_date); //obtener el intervalo para dias
+                $diasSeleccionados = $intervalo->days . ' Días'; // obtener el total de dias
+            }
+        }
     }
-}
-
-// calculo de dias seleccionados
-if($fechaInicio != '' && $fechaFin == ''){//funion de un solo dia
-    $diasSeleccionados = 1; //uno perteneciendo a solo un dia que se selecciono
-} else if ($fechaInicio != '' && $fechaFin != ''){ //funcion donde se selecciona un rango de fechas
-
 }
 
 
 
 $var = [
     'periodo' => $periodo,
-    'diasSeleccionados' => $diasSeleccionados = $diasSeleccionados > 1 ? $diasSeleccionados + ' Dias' : $diasSeleccionados + ' Dia',
+    'diasSeleccionados' => $diasSeleccionados,
+    'diasRestantes' => $diasRestantes,
 ];
 echo json_encode($var);
+
+
+function getAllDays($fechaInio, $fechaFin, $idEmployee) // la funcion obtiene el total de vacaciones de empleados
+{
+    $catDiasM = new CatDiasM();
+    $row = new row();
+
+    $result = $row->returnArrayById($catDiasM->getMoreDaysForP());
+    $days = $result[0];
+    if (pg_num_rows($catDiasM->getAllDays($fechaInio, $fechaFin, $idEmployee)) > 0) {
+        $result = $row->returnArrayById($catDiasM->getAllDays($fechaInio, $fechaFin, $idEmployee));
+        $days = $result[0];
+    }
+
+    return $days;
+}
--- App/Model/Catalogos/CatDiasM/CatDiasM.php
+++ App/Model/Catalogos/CatDiasM/CatDiasM.php
@@ -25,9 +25,38 @@ class CatDiasM
     ///GET PERIODO 
     public function getPeriodo($date){
         $query = pg_query ("SELECT 
-                                (central.cat_periodo.descripcion)
+                                (central.cat_periodo.descripcion),
+                                fecha_inicio,
+                                fecha_fin
                             FROM central.cat_periodo
                             WHERE '$date' BETWEEN fecha_inicio AND fecha_fin;");
+        return $query;
+    }
+
+    public function getAllDays($fechaInicio, $fechaFin, $idEmployee){
+        $query = pg_query ("SELECT
+                                SUM(
+                                    CASE
+                                        WHEN fecha_fin IS NOT NULL THEN
+                                            (fecha_fin - fecha_inicio)::int
+                                        ELSE
+                                            1
+                                    END
+                                ) AS total_dias
+                            FROM
+                                central.ctrl_incidencias
+                            WHERE 
+                                id_tbl_empleados_hraes = $idEmployee
+                            AND fecha_inicio BETWEEN '$fechaInicio' AND '$fechaFin';");
+        return $query;
+    }
+
+    public function getMoreDaysForP(){
+        $query = pg_query ("SELECT 
+                                central.cat_asistencia_config.dias_x_periodo
+                            FROM central.cat_asistencia_config
+                            WHERE central.cat_asistencia_config.id_cat_asistencia_config = 1 -- IS THE DATA FIRST
+                            LIMIT 1;");
         return $query;
     }
 }
