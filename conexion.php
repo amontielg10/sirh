0630eb5eb8337aef2109f70693c53c28a85f8a39
Aldair Trejo <147780739+aldairtrejot@users.noreply.github.com>
2024-10-11 09:09:37 -0600

    more changes

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
-$password = "pg2024";
+$password = "sirh2024";
 $username = "postgres";
-$dbname = "sirh";
+$dbname = "sirh_delete";
 $host = "localhost";
 $port = "5432";
 $options = "--client_encoding=UTF8";
@@ -10,7 +10,7 @@ try {
     $connectionDB = "host=$host port=$port dbname=$dbname user=$username password=$password options=$options";
     $connectionDBsPro = pg_pconnect($connectionDB);
 } catch (\Throwable $e) {
-    echo "Error connecting to data base + ".$e; 
+    echo "Error connecting to data base + " . $e;
 }
 
 /// Connection to server
